144627ddaa4d4d3d60769d962a0ae0b44afec172
SrLuc <ruanlucenaramos@gmail.com>
2025-02-18 15:40:54 -0300

    feat: adding ref and reactive functions

---
+++ vue-project/src/components/Card.vue
@@ -0,0 +1,51 @@
+<script  setup>
+import { ref } from 'vue'
+import { reactive } from 'vue';
+
+const age = ref(0)
+
+const years = reactive({
+    "year": 2024
+})
+
+function fazeraniversario() {
+    age.value++
+}
+
+function updateYear() {
+    years.year += 1
+}
+
+</script>
+
+<template>
+    <div>
+        <p>Idade Atual: {{ age }} </p>
+        <button @click="fazeraniversario">Fazer Anivesário</button>
+    </div>
+    <div>
+        <p>Ano Atual : {{ years.year }} </p>
+        <button @click="updateYear">Atualizar Ano</button>
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-color: #f9f9f9;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+}
+
+button {
+    background-color: #f9f9f9;
+    border: 1px solid #333;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin: 10px;
+}
+
+p {
+    margin: 0;
+}
+</style>
---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>Hello {{ name }} </h1>
+    </header>
+</template>
+
+
+<style scoped>
+header {
+    background-color: #333;
+    color: white;
+    padding: 10px;
+    text-align: center;
+}
+
+h1 {
+    margin: 0;
+}
+</style>
+
+
+<script setup>
+const name = "ruan"
+</script>
